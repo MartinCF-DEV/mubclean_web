814f18b9f4299e3d496a54ad385b92362851ed32
MartinC <cabreramartin362@gmail.com>
2026-01-29 17:40:39 -0600

    licencias para los admins

--- mubclean-frontend/src/app/public-license/public-license.component.ts
+++ mubclean-frontend/src/app/public-license/public-license.component.ts
@@ -1,3 +1,7 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-public-license',
   standalone: true,
